25ea94048a38d26369b821a41f10e8a491e9f259
jose.ra.87 <jose.ra.87@97c83ed2-6e0d-7015-3a71-91dbb3a07a5f>
2010-04-28 07:09:49 +0000

    Configurado el POM para ejecutar el proyecto Agregado el metodo que genera la matriz de Hadamard Agregada clase ejecutable de Prueba en el paquete de experimentos

---
+++ trunk/quantum4g/src/main/java/com/quantum4g/experimentos/principal/NewMain.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.quantum4g.experimentos.principal;
+
+import com.quantum4g.utilitarios.OperacionesMatrices;
+import com.quantum4g.core.entidades.EstadoCuantico;
+/**
+ *
+ * @author Pepe
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        OperacionesMatrices operacion=new OperacionesMatrices(2);
+        double[][] matrizHadamard=operacion.crearTransfHadamard(new  EstadoCuantico[1], 2);
+        
+        for (int i=0;i<matrizHadamard.length;i++){
+            for (int j=0;j<matrizHadamard.length;j++){
+                System.out.print(matrizHadamard[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+}
