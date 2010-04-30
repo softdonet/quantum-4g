58c2795867c60f4e1c41a7f413b30829af4ca3af
jose.ra.87 <jose.ra.87@97c83ed2-6e0d-7015-3a71-91dbb3a07a5f>
2010-04-30 19:48:45 +0000

    Implementando la operacion productoMatriz --- Parte 1

--- trunk/quantum4g/src/main/java/com/quantum4g/utilitarios/OperacionesMatrices.java
+++ trunk/quantum4g/src/main/java/com/quantum4g/utilitarios/OperacionesMatrices.java
@@ -1,4 +1,4 @@
-/*
+/*  
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -19,8 +19,6 @@ public class OperacionesMatrices {
     
     private int[][] matrizIdentidad;
 
-    private double[][] matrizHadamardInicial;
-
     private final double INVERSA_RAIZ=(1/Math.sqrt(2));
 
     public OperacionesMatrices(int N){
@@ -38,22 +36,15 @@ public class OperacionesMatrices {
                 }
             }
         }
-
-        this.matrizHadamardInicial=new double[2][2];
-        this.matrizHadamardInicial[0][0]=INVERSA_RAIZ;
-        this.matrizHadamardInicial[0][1]=INVERSA_RAIZ;
-        this.matrizHadamardInicial[1][0]=INVERSA_RAIZ;
-        this.matrizHadamardInicial[1][1]=INVERSA_RAIZ*(-1);
     }
 
-    public double[][] crearTransfHadamard(EstadoCuantico[] vectorEstados, int N) {
+    public double[][] crearTransfHadamard(int N) {
         double[][] matrizHadamard=null;
         int tamMatriz=(int) Math.pow(2,N);
         matrizHadamard=new double[tamMatriz][tamMatriz];
 
           for (int i=0;i<N*2;i++){
                 for (int j=0;j<N*2;j++){
-
                     matrizHadamard[i][j]=(1/(Math.pow(2,N/2)))*Math.pow(-1,productoPuntoBinario(i,j));
                 }
           }
@@ -61,30 +52,25 @@ public class OperacionesMatrices {
         return matrizHadamard;
     }
 
-//    private double[][] formaMatrizHadamard(double[][] matrizHadamardAnterior, int N) {
-//
-//        double[][] matrizHadamardActual= new double[N*2][N*2];
-//
-//        for (int i=0;i<N*2;i++){
-//                for (int j=0;j<N*2;j++){
-//                    if (i<N && j<N){
-//                        matrizHadamardActual[i][j]=matrizHadamardAnterior[i][j]*INVERSA_RAIZ;
-//                    }
-//                    if (i>=N && j<N){
-//                        matrizHadamardActual[i][j]=matrizHadamardAnterior[i-N][j]*INVERSA_RAIZ;
-//                    }
-//                    if (i<N && j>=N){
-//                        matrizHadamardActual[i][j]=matrizHadamardAnterior[i][j-N]*INVERSA_RAIZ;
-//                    }
-//                    if (i>=N && j>=N){
-//                        matrizHadamardActual[i][j]=matrizHadamardAnterior[i-N][j-N]*INVERSA_RAIZ*(-1);
-//                    }
-//                }
-//        }
-//        return matrizHadamardActual;
-//    }
+    private int productoPuntoBinario(int i, int j) {
+        int suma=0;
+        for (int k=0;k<N;k++){
+            suma+= ((i & (int)(Math.pow(2,k)))>>k)*((j & (int) Math.pow(2,k))>>k);
+        }
+        return suma;
+    }
 
-    public EstadoCuantico[] productoMatrizVector(EstadoCuantico[][] matrizGrover, EstadoCuantico[] estadosBaseOraculo) {
+    public EstadoCuantico[] productoMatrizVector(EstadoCuantico[][] matriz, EstadoCuantico[] vector) {
+        int tamVector=vector.length;
+        //EstadoCuantico[] resultadoProducto=new EstadoCuantico[tamVector];
+        double valorParcial;
+        for (int i=0;i<tamVector;i++){
+            valorParcial=0;
+            for (int j=0;j<tamVector;j++){
+                valorParcial+=matriz[i][j].getValorMatriz()*vector[j].getValorMatriz();
+            }
+        }
+
         return null;
     }
 
@@ -111,13 +97,4 @@ public class OperacionesMatrices {
     public void setMatrizIdentidad(int[][] matrizIdentidad) {
         this.matrizIdentidad = matrizIdentidad;
     }
-
-    private int productoPuntoBinario(int i, int j) {
-        int suma=0;
-        for (int k=0;k<N;k++){
-            suma+= ((i & (int)(Math.pow(2,k)))>>k)*((j & (int) Math.pow(2,k))>>k);
-        }
-        return suma;
-    }
-
 }
