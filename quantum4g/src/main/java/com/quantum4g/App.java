3d74919c74a703d940dea5784252b3c9b96d4676
jose.ra.87 <jose.ra.87@97c83ed2-6e0d-7015-3a71-91dbb3a07a5f>
2010-06-03 04:13:07 +0000

    Actualizado App.java con invocaciones de funciones para depurar

--- quantum4g/src/main/java/com/quantum4g/App.java
+++ quantum4g/src/main/java/com/quantum4g/App.java
@@ -1,13 +1,27 @@
 package com.quantum4g;
 
-/**
- * Hello world!
- *
- */
+import com.quantum4g.algoritmos.grasp.AlgoritmoGRASP;
+import com.quantum4g.algoritmos.grover.AlgoritmoGrover;
+import com.quantum4g.experimentos.principal.Principal;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+
+          Principal principal=new Principal(10);
+          AlgoritmoGrover algoritmoGrover=new AlgoritmoGrover(10, principal.inicializaUniverso());
+          algoritmoGrover.ejecucionGrover();
+
+          AlgoritmoGRASP algoritmoGRASP=new AlgoritmoGRASP(10, principal.generaValoresTriadaGen(10));
+          algoritmoGRASP.ejecucionGRASP();
+          //        OperacionesMatrices operacion=new OperacionesMatrices(2);
+//        double[][] matrizHadamard=operacion.crearTransfHadamard( 2);
+//
+//        for (int i=0;i<matrizHadamard.length;i++){
+//            for (int j=0;j<matrizHadamard.length;j++){
+//                System.out.print(matrizHadamard[i][j]+" ");
+//            }
+//            System.out.println();
+//        }
     }
 }
