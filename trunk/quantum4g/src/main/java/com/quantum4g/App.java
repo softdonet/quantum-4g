faed43e0248ee854cd1ba1f6d21f5d1e9f699540
jose.ra.87 <jose.ra.87@97c83ed2-6e0d-7015-3a71-91dbb3a07a5f>
2010-06-26 05:54:26 +0000

    Corregido error en la implementación del Algoritmo Grover
    Algoritmo Grover supera ahora al algoritmo Grasp en una proporción de 3 a 1

--- trunk/quantum4g/src/main/java/com/quantum4g/App.java
+++ trunk/quantum4g/src/main/java/com/quantum4g/App.java
@@ -4,16 +4,24 @@ import com.quantum4g.algoritmos.grasp.AlgoritmoGRASP;
 import com.quantum4g.algoritmos.grover.AlgoritmoGrover;
 import com.quantum4g.core.entidades.Triada;
 import com.quantum4g.experimentos.principal.Principal;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 public class App 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
           double resultadoGrover;
           double resultadoGrasp;
           double groverWins=0;
           double graspWins=0;
 
-          for (int i=0;i<100;i++)
+          Writer out = new OutputStreamWriter(new FileOutputStream("./resultadosExperimento.txt"));
+
+          int cantidadIteraciones=100;
+          for (int i=0;i<cantidadIteraciones;i++)
           {
               Principal principal=new Principal(10);
               Triada[] triadaExperimento= principal.generaValoresTriadaGen(10);
@@ -23,6 +31,10 @@ public class App
               //Ejecucion Grover
               AlgoritmoGrover algoritmoGrover=new AlgoritmoGrover(10, principal.inicializaUniverso(triadaExperimento));
               resultadoGrover=algoritmoGrover.ejecucionGrover();
+
+              out.write("Iteracion : " + i + " de " + cantidadIteraciones + "\n");
+              out.write("Algoritmo Grasp: "+resultadoGrasp + "\n");
+              out.write("Algoritmo Grover: "+resultadoGrover + "\n\n");
 
               if (resultadoGrover>resultadoGrasp){
                   groverWins++;
