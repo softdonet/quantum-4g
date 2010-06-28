b2fb88251377c812cedf609fb56c83f1351ba16e
jose.ra.87 <jose.ra.87@97c83ed2-6e0d-7015-3a71-91dbb3a07a5f>
2010-06-28 05:30:04 +0000

    Actualizado experimento principal

--- quantum4g/src/main/java/com/quantum4g/App.java
+++ quantum4g/src/main/java/com/quantum4g/App.java
@@ -4,6 +4,7 @@ import com.quantum4g.algoritmos.grasp.AlgoritmoGRASP;
 import com.quantum4g.algoritmos.grover.AlgoritmoGrover;
 import com.quantum4g.core.entidades.Triada;
 import com.quantum4g.experimentos.principal.Principal;
+import java.io.DataOutputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -18,7 +19,12 @@ public class App
           double groverWins=0;
           double graspWins=0;
 
-          Writer out = new OutputStreamWriter(new FileOutputStream("./resultadosExperimento.txt"));
+          DataOutputStream out1 = new DataOutputStream(new FileOutputStream("./resultadosExperimento.txt"));
+          DataOutputStream out2 = new DataOutputStream(new FileOutputStream("./resultadosGrover.txt"));
+          DataOutputStream out3 = new DataOutputStream(new FileOutputStream("./resultadosGrasp.txt"));
+
+          out2.writeBytes("Algoritmo Grover: \n\n");
+          out3.writeBytes("Algoritmo Grasp: \n\n");
 
           int cantidadIteraciones=100;
           for (int i=0;i<cantidadIteraciones;i++)
@@ -32,20 +38,42 @@ public class App
               AlgoritmoGrover algoritmoGrover=new AlgoritmoGrover(10, principal.inicializaUniverso(triadaExperimento));
               resultadoGrover=algoritmoGrover.ejecucionGrover();
 
-              out.write("Iteracion : " + i + " de " + cantidadIteraciones + "\n");
-              out.write("Algoritmo Grasp: "+resultadoGrasp + "\n");
-              out.write("Algoritmo Grover: "+resultadoGrover + "\n\n");
+              out1.writeBytes("Datos de Entrada: \n\n");
+
+              for (int h=0;h<triadaExperimento.length;h++){
+                  out1.writeBytes("Triada "+h + ":\n");
+                  out1.writeBytes("Factor Positivo: "+ triadaExperimento[h].getFactorPositivo() + ":\n");
+                  out1.writeBytes("Factor Negativo: "+ triadaExperimento[h].getFactorNegativo() + ":\n");
+                  out1.writeBytes("Factor Ponderación: "+ triadaExperimento[h].getFactorPonderacion() + ":\n\n");
+              }
+
+              out1.writeBytes("Datos de Salida: \n\n");
+              out1.writeBytes("Iteracion : " + i + " de " + cantidadIteraciones+ "\n");
+              out1.writeBytes("Algoritmo Grasp: "+resultadoGrasp + "\n");
+              out1.writeBytes("Algoritmo Grover: "+resultadoGrover + "\n\n");
+
+              out2.writeBytes(resultadoGrover + "\n");
+              out3.writeBytes(resultadoGrasp + "\n");
 
               if (resultadoGrover>resultadoGrasp){
                   groverWins++;
+                  out1.writeBytes("GROVER ganó por : "+(resultadoGrover-resultadoGrasp) + "\n\n");
               }
               else{
                   graspWins++;
+                  out1.writeBytes("GRASP ganó por : "+(resultadoGrasp-resultadoGrover) + "\n\n");
               }
+
+              out1.writeBytes("----------------------------------------------------------------------- \n\n");
+
           }
           System.out.println("\n\n\n");
           System.out.println("Victorias GROVER: "+groverWins);
           System.out.println("Victorias GRASP: "+graspWins);
+          out1.close();
+          out2.close();
+          out3.close();
 
+          //TODO- Medir la eficiencia
     }
 }
